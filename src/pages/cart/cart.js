19355be2ee00825331647a3721f3df39a4ab4f8c
bieucbt <bieucbt@gmail.con>
2023-06-23 15:18:57 +0700

    update

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import ProductCart from '../../components/productCart/productCart'
 import {productsData} from '../../Data/productsData'
-import {CartContext} from '../../context/shopContext'
 
 
 export const Cart = () => {
